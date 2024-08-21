dbfc1644c028badc9ab54c3e15362d59e3379968
sunbear <dbwofldb@naver.com>
2024-08-21 09:41:37 +0900

    dmdkd

---
+++ rush01/back.c
@@ -0,0 +1,23 @@
+void    nb(int i)
+{
+    int j;
+
+    if (promising(i))
+    {
+        fuc_arr();
+        return
+    }
+    {
+        else
+        {
+            j = 1;
+            while (j == n)
+            {
+                i++;
+                nb()
+                j++;
+            }
+            
+        }
+    }
+}
