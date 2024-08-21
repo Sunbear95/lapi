9a857d764ad0bbcf88ddbafbfd60ffb60d905c84
sunbear <dbwofldb@naver.com>
2024-08-21 14:34:46 +0900

    dkf

---
+++ rush01/arry&nqueen/arr 경우의 수/div.c
@@ -0,0 +1,10 @@
+
+void div (int a, int b, int i)
+{
+    if (a + b == 5)
+        fuc_arr(a, b);
+    else if (a + b == 4)
+        fuc_arr1(a, b);
+    else if (a + b == 3)
+        fuc_arr2(a, b);   
+}
