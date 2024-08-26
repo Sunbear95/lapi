39879007e9f982b9e82045bca256a4fbbc08b652
sunbear <dbwofldb@naver.com>
2024-08-26 23:28:14 +0900

    gg~

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -6,19 +6,23 @@
 /*   By: jyoo <jyoo@student.42gyeonsan.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 22:48:04 by jyoo              #+#    #+#             */
-/*   Updated: 2024/08/25 23:11:56 by jyoo             ###   ########.fr       */
+/*   Updated: 2024/08/26 20:09:47 by jyoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
+#include <unistd.h>
 
 int	ft_ultimate_range(int **range, int min, int max)
 {
 	int	i;
 
 	if (max <= min)
+	{
+		*range = NULL;
 		return (0);
+	}
 	i = 0;
-	*range = (int *)malloc(sizeof(int) * (max - min + 1));
+	*range = (int *)malloc(sizeof(int) * (max - min));
 	while (i < (max - min))
 	{
 		(*range)[i] = min + i;
---
+++ C07/ex05/ft_split.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyoo <jyoo@student.42gyeonsan.kr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 20:10:50 by jyoo              #+#    #+#             */
+/*   Updated: 2024/08/26 22:16:01 by jyoo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	nword(char *str)
+{
+	int i;
+	int	up;
+	int	num;
+	
+	i = 0;
+	up = 1;
+	while (str[i])
+	{
+		while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		{
+			up = 1;
+			i++;
+		}
+		while (up = 1 && !(str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+		{
+			up = 0;
+			i++;
+			num++;
+		}
+	}
+	return (num);
+}
+
+char **ft_malloc(arr, i ,k)
+	int	j;
+
+	j = 0
+
+char **ft_split(char *str, char *charset)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	**arr;
+
+	i = 0;
+	while (i < nword(str))
+	{
+		j = 0;
+		while (str[j])
+		{
+			while (str[j] == ' ' || str[j] == '\t' || str[j] == '\n')
+				j++;
+			k = 0;
+			while (!(str[j + k] == ' ' || str[j + k] == '\t' || str[j + k] == '\n')
+				k++;
+			ft_malloc(arr, i , k);
+				arr[i] = (char *)malloc(sizeof(char) * (k + 1);
+			while (< k
