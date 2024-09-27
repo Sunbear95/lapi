cd86d47f8d23241204acaed943e1bc5e8003a80f
sunbear <dbwofldb@naver.com>
2024-09-27 17:59:20 +0900

    zip

--- C_reload/ex18/ft_print_params.c
+++ C_reload/ex18/ft_print_params.c
@@ -1,53 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jyoo <jyoo@student.42gyeonsan.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/24 14:26:56 by jyoo              #+#    #+#             */
-/*   Updated: 2024/08/25 22:03:15 by jyoo             ###   ########.fr       */
+/*   Created: 2024/08/24 14:14:41 by jyoo              #+#    #+#             */
+/*   Updated: 2024/09/27 16:15:35 by jyoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_swap(char **argv, int i, int j)
-{
-	char	*temp;
-
-	temp = argv[i];
-	argv[i] = argv[j];
-	argv[j] = temp;
-}
-
-void	ft_strcmp(int argc, char **argv)
-{
-	int	i;
-	int	j;
-	int	k;
-
-	i = 1;
-	while (i + 1 < argc)
-	{
-		j = i + 1;
-		while (j < argc)
-		{	
-			k = 0;
-			while (argv[i][k] == argv[j][k] && argv[i][k] && argv[j][k])
-				k++;
-			if (argv[i][k] > argv[j][k])
-				ft_swap(argv, i, j);
-			j++;
-		}
-		i++;
-	}
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
 int	ft_strlen(char *str)
 {
@@ -64,7 +29,6 @@ int	main(int argc, char **argv)
 	int	i;
 	int	temp;
 
-	ft_strcmp(argc, argv);
 	i = 1;
 	while (i < argc)
 	{
