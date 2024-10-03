69232842eb314673f1939eb566f855756db879ec
sunbear <dbwofldb@naver.com>
2024-10-03 11:50:26 +0900

    why

---
+++ 42_C/libft/func/ft_strlcpy.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyoo <jyoo@student.42gyeonsan.kr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 11:12:12 by jyoo              #+#    #+#             */
+/*   Updated: 2024/10/03 11:13:24 by jyoo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	length;
+
+	length = 0;
+	while (str[length] != 0)
+	{
+		length++;
+	}
+	return (length);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	src_len;
+
+	src_len = ft_strlen(src);
+	i = 0;
+	if (size == 0)
+		return (src_len);
+	while (src[i] != 0 && i < (size - 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (size > 0)
+		dest[i] = 0;
+	return (src_len);
+}
