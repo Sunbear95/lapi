c909c53e5b6fa58aaa6e6b7ba3e71fc1245c7d33
sunbear <dbwofldb@naver.com>
2024-10-05 18:07:46 +0900

    123

--- 42_C/libft/func/strtrim.c
+++ 42_C/libft/func/strtrim.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   strtrim.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jyoo <jyoo@student.42gyeonsan.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/05 17:16:17 by jyoo              #+#    #+#             */
-/*   Updated: 2024/10/05 17:53:46 by jyoo             ###   ########.fr       */
+/*   Created: 2024/10/05 17:55:17 by jyoo              #+#    #+#             */
+/*   Updated: 2024/10/05 18:07:23 by jyoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strtrim(char const *s1, char const *set)
 {
+	int	i;
 	char	*temp;
 
-	temp = (char *)calloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)));
-	temp = ft_strncpy(temp, s1, ft_strlen(s1));
-	temp = ft_strncpy(temp[ftstrlen(s1)], s2, ft_strlen(s2));
-	return (temp);
-}
+	temp = (char *) calloc(sizeof(char) * (ft_strlen(s1)));
+	i = -1;
+	while (++i < ft_strlen(s1))
+	
