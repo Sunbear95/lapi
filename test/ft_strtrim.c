8b351776b9eeda1d0f2497c8af27d53ab5b3380e
sunbear <dbwofldb@naver.com>
2024-10-18 00:50:55 +0900

    2024.10.18

--- test/ft_strtrim.c
+++ test/ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jyoo <jyoo@student.42gyeonsan.kr>          +#+  +:+       +#+        */
+/*   By: jyoo <jyoo@student.42gyeongsan.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 17:55:17 by jyoo              #+#    #+#             */
-/*   Updated: 2024/10/17 19:25:20 by jyoo             ###   ########.fr       */
+/*   Updated: 2024/10/18 00:48:42 by jyoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,7 @@
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*temp;
-	size_t	len;
-	size_t	i;
-	size_t	j;
 
-	len = ft_strlen(s1);
-	if (len == 0)
-		return (0);
 	temp = ft_strdup(s1);
-	i = -1;
-	while (set[++i])
-	{
-		if (s1[i] != set[i])
-			break ;
-		temp++;
-	}
-	j = -1;
-	while (set[++j])
-	{
-		if (s1[len - j - 1 - i] == set[j])
-			break ;
-		temp[len - j - 1 - i] = '\0';
-	}
-	return (temp);
+	return (0);
 }
