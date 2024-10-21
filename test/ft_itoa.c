941eeff1ffe5e9e4ddd163ccc91dea80148af31e
sunbear <dbwofldb@naver.com>
2024-10-22 04:02:03 +0900

    late

--- test/ft_itoa.c
+++ test/ft_itoa.c
@@ -3,72 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jyoo <jyoo@student.42gyeongsan.kr>         +#+  +:+       +#+        */
+/*   By: jyoo <jyoo@student.42gyeonsan.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/07 13:51:13 by jyoo              #+#    #+#             */
-/*   Updated: 2024/10/21 00:39:41 by jyoo             ###   ########.fr       */
+/*   Created: 2024/10/21 23:24:30 by jyoo              #+#    #+#             */
+/*   Updated: 2024/10/22 00:41:25 by jyoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-int	posn(int n)
+size_t	ft_numleng(int n)
 {
-	if (n < 0)
-		return (-n);
-	if (n >= 0)
-		return (n);
-	return (n);
+	size_t	i;
+
+	i = 0;
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
 }
 
-int	num_leng(int num)
+char	*ft_itoasub(int n, int len, char *arr)
 {
-	int		leng;
+	int		i;
 
-	leng = 0;
-	if (num == 0)
-		return (1);
-	while (num != 0)
+	if (n == -2147483648)
+		return ("-2147483648");
+	if (n < 0)
 	{
-		leng++;
-		num /= 10;
+		n *= -1;
+		arr[0] = '-';
+		len++;
 	}
-	return (leng);
+	i = 0;
+	while (n != 0)
+	{
+		arr[len - i - 1] = n % 10 + '0';
+		n /= 10;
+		i++;
+	}
+	arr[len] = '\0';
+	return (arr);
 }
 
 char	*ft_itoa(int n)
 {
-	char	*temp;
-	int		num;
+	char	*arr;
 	int		len;
 
-	len = num_leng(n);
-	if (n < 0)
-	{
-		if (n == -2147483648)
-			return (strdup("-2147483648"));
-		len++;
-		temp = (char *)calloc((len + 1), sizeof (char));
-		temp[0] = '-';
-	}
-	else
-		temp = (char *)calloc((len + 1), sizeof (char));
-	num = posn(n);
-	temp[len] = '\0';
-	while (len-- > 0)
-	{
-		if (n < 0 && len == 0)
-			break ;
-		temp[len] = (num % 10) + '0';
-		num /= 10;
-	}
-	return (temp);
-}
-
-int	main ()
-{
-	printf("%s", ft_itoa(123));
-	return (0);
+	if (n == 0)
+		return ("0");
+	len = ft_numleng(n);
+	arr = (char *)ft_calloc(len + 1, sizeof (char));
+	arr = ft_itoasub(n, len, arr);
+	return (arr);
 }
--- test/ft_putnbr_fd.c
+++ test/ft_putnbr_fd.c
@@ -6,22 +6,56 @@
 /*   By: jyoo <jyoo@student.42gyeongsan.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:56:10 by jyoo              #+#    #+#             */
-/*   Updated: 2024/10/18 15:11:56 by jyoo             ###   ########.fr       */
+/*   Updated: 2024/10/22 04:01:27 by jyoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <unistd.h>
+#include "libft.h"
+
+int	ft_power(int n, int time)
+{
+	int		temp;
+
+	temp = 1;
+	while (time-- > 0)
+		temp = temp * n;
+	return (temp);
+}
+
+int	ft_numleng(int n)
+{
+	int		temp;
+
+	temp = 0;
+	while (n != 0)
+	{	
+		n /= 10;
+		temp++;
+	}
+	return (temp);
+}
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*temp;
+	char	c;
 	int		len;
-	int		i;
 
-	temp  = ft_itoa(n);
-	len = ft_strlen(temp);
-	i = -1;
-	while (++i < len)
-		write (fd, &temp[i], 1);
+	if (n == -2147483648)
+		write (fd, "-2147483648", 11);
+	if (n == 0)
+		write (fd, "0", 1);
+	len = ft_numleng(n);
+	if (n < 0)
+	{
+		write (fd, "-", 1);
+		n *= -1;
+	}
+	while (n != 0)
+	{
+		c = (n / ft_power(10, len - 1)) + '0';
+		n = n % ft_power(10, len - 1);
+		write (fd, &c, 1);
+		len--;
+	}
 }
